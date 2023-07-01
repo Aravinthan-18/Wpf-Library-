e96b5f620f2aabc96c46a16f1fc051b6618f4e7a
Aravinthan <134296054+Aravinthan-18@users.noreply.github.com>
2023-07-01 11:56:02 +0530

    Add files via upload

---
+++ Books.xaml.cs
@@ -0,0 +1,222 @@
+﻿using Library.Set;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for Books.xaml
+    /// </summary>
+    public partial class Books : Window
+    {
+        public Books()
+        {
+            InitializeComponent();
+            Set.Settings1 settings1= new Set.Settings1();
+            lblname.Content = settings1.You;
+            lblponni.Content = settings1.Ponni;
+            lblpython.Content = settings1.Python;
+            lblcpro.Content = settings1.cpro;
+            lblcsharp.Content = settings1.csharp;
+            lbljava.Content=settings1.java;
+            lblche.Content=settings1.che;
+            lblphy.Content=settings1.phy;
+            lbltes.Content=settings1.tes;
+        }
+        Set.Settings1 settings1 = new Set.Settings1();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblponni.Content = settings1.Ponni;
+            settings1.Ponni -=1;
+            settings1.Save();
+            {
+                lblponni.Content = settings1.Ponni;
+                MessageBox.Show("Ponniyin Selvan","Library");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lblponni.Content = settings1.Ponni;
+            settings1.Ponni += 1;
+            settings1.Save();
+            {
+                lblponni.Content = settings1.Ponni;
+                MessageBox.Show("Ponniyin Selvan", "Library");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lblpython.Content = settings1.Python;
+            settings1.Python -= 1;
+            settings1.Save();
+            {
+                lblpython.Content = settings1.Python;
+                MessageBox.Show("Python", "Library");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            lblpython.Content = settings1.Python;
+            settings1.Python += 1;
+            settings1.Save();
+            {
+                lblpython.Content = settings1.Python;
+                MessageBox.Show("Python", "Library");
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            lblcpro.Content = settings1.cpro;
+            settings1.cpro -= 1;
+            settings1.Save();
+            {
+                lblcpro.Content = settings1.cpro  ;
+                MessageBox.Show("Cprogram", "Library");
+            }
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            lblcpro.Content = settings1.cpro;
+            settings1.cpro += 1;
+            settings1.Save();
+            {
+                lblcpro.Content = settings1.cpro;
+                MessageBox.Show("Cprogram", "Library");
+            }
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            lblcsharp.Content = settings1.csharp;
+            settings1.csharp -= 1;
+            settings1.Save();
+            {
+                lblcsharp.Content = settings1.csharp;
+                MessageBox.Show("Csharp", "Library");
+            }
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            lblcsharp.Content = settings1.csharp;
+            settings1.csharp += 1;
+            settings1.Save();
+            {
+                lblcsharp.Content = settings1.csharp;
+                MessageBox.Show("Csharp", "Library");
+            }
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            lbljava.Content = settings1.java;
+            settings1.java -= 1;
+            settings1.Save();
+            {
+                lbljava.Content = settings1.java;
+                MessageBox.Show("Java", "Library");
+            }
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            lbljava.Content = settings1.java;
+            settings1.java += 1;
+            settings1.Save();
+            {
+                lbljava.Content = settings1.java;
+                MessageBox.Show("Java", "Library");
+            }
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            lblche.Content = settings1.java;
+            settings1.che -= 1;
+            settings1.Save();
+            {
+                lblche.Content = settings1.che;
+                MessageBox.Show("Chemistry", "Library");
+            }
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            lblche.Content = settings1.che;
+            settings1.che += 1;
+            settings1.Save();
+            {
+                lblche.Content = settings1.che;
+                MessageBox.Show("Chemistry", "Library");
+            }
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            lblphy.Content = settings1.phy;
+            settings1.phy -= 1;
+            settings1.Save();
+            {
+                lblphy.Content = settings1.phy;
+                MessageBox.Show("Physics", "Library");
+            }
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            lblphy.Content = settings1.phy;
+            settings1.phy += 1;
+            settings1.Save();
+            {
+                lblphy.Content = settings1.phy;
+                MessageBox.Show("Physics", "Library");
+            }
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            lbltes.Content = settings1.tes;
+            settings1.tes -= 1;
+            settings1.Save();
+            {
+                lbltes.Content = settings1.tes;
+                MessageBox.Show("Tesla", "Library");
+            }
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            lbltes.Content = settings1.tes;
+            settings1.tes += 1;
+            settings1.Save();
+            {
+                lbltes.Content = settings1.tes;
+                MessageBox.Show("Tesla", "Library");
+            }
+        }
+
+        private void Button_Click_16(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Logout conformation","Information");
+               Application.Current.Shutdown();
+            
+        }
+    }
+}
---
+++ Register.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Configuration;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        string Rootpath;
+        public Register()
+        {
+            InitializeComponent();
+            Rootpath = "C:\\Users\\welcome\\Documents\\";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                Set. Settings1 settings = new Set.Settings1();
+                settings.You = txtname.Text;
+                settings.Save();
+            {
+
+                if (txtname.Text != "" && txtcpass.Text != "" && txtmail.Text != "" && txtusname.Text != "")
+                {
+                    string Newfoldername = System.IO.Path.Combine(Rootpath, txtname.Text);
+                    Directory.CreateDirectory(Newfoldername);
+                    string Content = $"{txtname.Text}|{txtmail.Text}|{txtpass.Text}";
+                    MessageBoxResult result=MessageBox.Show("Register sucessful");
+                    string Create=System.IO.Path.Combine(Newfoldername,"Profile");
+                    File.WriteAllText(Create,Content);
+                     if (result == MessageBoxResult.OK)
+
+                    {
+                        Register register = new Register();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Fill all Details");
+                    }
+                }
+               
+                }
+                }
+
+      
+
+        private void txtusname_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtusname.Clear();
+        }
+
+        private void txtname_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtname.Clear();
+        }
+
+        private void txtmail_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtmail.Clear();    
+        }
+
+        private void txtpass_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtpass.Clear();
+        }
+
+        private void txtcpass_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtcpass.Clear();
+        }
+    }
+        }
+    
+
